6af3a488fff7f492bd88054d7411b435acb0ecaf
sergioabv2 <159508948+sergioabv2@users.noreply.github.com>
2024-04-02 23:57:43 -0600

    Update fileupload.php

--- fileupload.php
+++ fileupload.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="msapplication-tap-highlight" content="no"/>
@@ -2899,6 +2901,6 @@ function mp3EncoderWorker () {
 
  </script>  
       
-
+</html>
 
       
