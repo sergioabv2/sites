f066fb3d22135229b99df4a416da90b9e89b5217
sergioabv2 <159508948+sergioabv2@users.noreply.github.com>
2024-05-14 18:03:35 -0600

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-app-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging-compat.js')
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDL3mXVkWVNHNb5BvG94M1E18_NiOgn4qQ",
+  authDomain: "notifications-3e3bd.firebaseapp.com",
+  projectId: "notifications-3e3bd",
+  storageBucket: "notifications-3e3bd.appspot.com",
+  messagingSenderId: "205309097881",
+  appId: "1:205309097881:web:b314395b0beaaa740a0508",
+  measurementId: "G-7E5QXN171E"
+};
+const app = firebase.initializeApp(firebaseConfig)
+const messaging = firebase.messaging()
